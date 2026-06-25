933ff3f793341c8ba6437e79ca610e0577106ba9
sruthi37 <146318561+sruthi37@users.noreply.github.com>
2026-06-25 12:42:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -60,5 +60,8 @@ def match_schemes():
     return jsonify(matched)
 
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
